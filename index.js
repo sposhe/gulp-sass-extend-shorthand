1da47af55ccafc2617981661a242eaeafdf04ee4
sposhe <github@seanoshea.me>
2020-07-25 09:10:02 -0400

    Fix adjascent shorthands not compiling

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const replace = require('gulp-replace')
 
-const findSugar = new RegExp(/([{;])\s*\n(\s*)([%\.])([^{;!]+)\s*(!optional)?;/, 'g')
+const findSugar = new RegExp(/(?<=[{;])\s*\n(\s*)([%\.])([^{;!]+)\s*(!optional)?;/, 'g')
 
-function meltSugar(match, p1, p2, p3, p4, p5) {
-  let sugar = (p3 + p4)
+function meltSugar(match, p1, p2, p3, p4) {
+
+  let sugar = (p2 + p3)
     .replace(/\s+/g,'')
     .split(',')
-    .map(val => p2 + '@extend ' + val  + (p5 ? ' ' + p5 : ''))
+    .map(val => p1 + '@extend ' + val  + (p4 ? ' ' + p4 : ''))
     .join(';\n')
-  return p1 + '\n' + sugar + ';'
+  return '\n' + sugar + ';'
 }
 
 module.exports = () => replace(findSugar, meltSugar)
